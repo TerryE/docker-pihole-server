d9b31b2978c3174264c14530d75f0950b317eaa6
terry <Terry@ellisons.org.uk>
2023-04-08 17:15:10 +0100

    Treaks for OpenVPN

--- vpn/target-bootstrap.sh
+++ vpn/target-bootstrap.sh
@@ -3,26 +3,28 @@
 # In-target initial provisioning of the Alpine OpenVPN LXC
 
 cd /usr/local
+set -vx
 
-PACKAGES='logrotate iputils nmap procps sudo tar tree util-linux xz'
+ln -s /usr/local/data/terry /home
+
+PACKAGES='dropbear iputils nmap procps sudo tar tree util-linux xz'
 apk add --no-cache ${PACKAGES}
 
 #Add VPN User account which is needed for the PiVPN install but with SSH keyed login only
 
 VPN_RNDPASS=$(head -c 64  </dev/urandom | tr -cd 'A-Za-z0-9#%&()*+,-.:<=>?@^_~')
-VPN_HOME="/home/$VPN_USER"
+echo -e "${VPN_RNDPASSD}\n${VPN_RNDPASSD}" | adduser -g '' -s /bin/bash  $VPN_USER
 
-addgroup -g $VPN_UID $VPN_USER
-echo -e "${VPN_RNDPASSD}\n${VPN_RNDPASSD}" | \
-  adduser -h $VPN_HOME -g '' -s /bin/bash -G $VPN_USER -u $VPN_UID $VPN_USER
-echo -e "$VPN_USER ALL=(ALL:ALL) NOPASSWD: ALL\n" > /etc/sudoers
-
+echo -e "$VPN_USER ALL=(ALL:ALL) NOPASSWD: ALL\n" >> /etc/sudoers
 mkdir -m 700 $VPN_HOME/.ssh
 cp -p /root/.ssh/authorized_keys $VPN_HOME/.ssh
 chown $VPN_USER:$VPN_USER -R $VPN_HOME/.ssh
 
 # Configure OpenVPN and ensure it is started after a reboot
 
-wget -O /tmp/install.sh https://install.pivpn.io
-chmod 755 /tmp/install.sh
-/tmp/install.sh --unattended /usr/local/conf/pivpn-init.conf
+wget -qO - https://install.pivpn.io | bash -s - --unattended /usr/local/conf/pivpn-init.conf
+
+cd /home/$VPN_USER
+last_backup=$(ls -l pivpnbackup | tail -1 | sed  's/.*:.. //') || exit
+service openvpn stop
+tar -C / -xzpf pivpnbackup/$last_backup
